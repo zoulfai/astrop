876c8173b8495526b632c1326afe8ae216fab896
zoulfai <72633607+zoulfai@users.noreply.github.com>
2024-09-28 00:24:54 +0400

    v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  site: 'https://zoulfai.github.io',
-  base: '/astrop',
+  site: 'https://zoulfai.github.io/astrop/',
+  base: 'my-repo',
 })
